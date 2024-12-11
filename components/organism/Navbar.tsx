52f6e0441067d9c4130c0ecc2e0ecf9028b9ea81
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-11 14:03:41 +0700

    feat: navbar fix

--- components/organism/Navbar.tsx
+++ components/organism/Navbar.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <>
-      <div className="flex flex-row items-center justify-between px-4 mx-auto border-gray-100 shadow-xl border-2 p-2 rounded-full w-1/2 my-8">
+      <div className="flex flex-row items-center justify-between px-8 mx-auto border-gray-100 shadow-xl border-2 p-2 rounded-full w-2/3 my-8">
         <Image src={logo} alt="logo" height={50} width={50} />
-        <ul className="flex flex-row items-center justify-center gap-8 font-medium">
+        <ul className="flex flex-row items-center justify-center space-x-16 font-medium">
           <li>Home</li>
           <li>About</li>
           <li>Feature</li>
