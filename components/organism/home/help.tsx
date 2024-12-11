891758d43be6beb09be759ac75de669e71d2ffc1
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-11 18:43:08 +0700

    add: help not already yet

--- components/organism/home/help.tsx
+++ components/organism/home/help.tsx
@@ -3,15 +3,23 @@ import phonecombine from "/public/images/phone-combine.png";
 export default function Help() {
   return (
     <>
-      <div className="bg-[#C7E4DF] w-3/5">
+      <div className="bg-[#C7E4DF] w-3/5 flex flex-row items-center mx-auto rounded-xl p-4">
         <div>
-          <h1></h1>
+          <h1 className="font-semibold text-5xl">
+            Help you find the finest quality of mental health support!
+          </h1>
+          <p className="md:w-3/4 text-lg mt-4">
+            Dopamind offers an extensive range of mental health solutions,
+            ensuring you get personalized care. Our advanced emotion recognition
+            feature helps match you with the right mental health professional
+            for your needs, promising an exceptional therapeutic experience.
+          </p>
         </div>
         <Image
           src={phonecombine}
           alt="phone-combine"
-          width={150}
-          height={150}
+          width={250}
+          height={250}
         />
       </div>
     </>
