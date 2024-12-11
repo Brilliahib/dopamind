1c6ede2e7ae45c8a48583b864f838049ee68c367
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-11 18:49:49 +0700

    fix: help

--- components/organism/home/help.tsx
+++ components/organism/home/help.tsx
@@ -3,9 +3,9 @@ import phonecombine from "/public/images/phone-combine.png";
 export default function Help() {
   return (
     <>
-      <div className="bg-[#C7E4DF] bg-opacity-50 w-3/5 flex flex-row items-center mx-auto rounded-3xl p-4 my-12">
+      <div className="bg-[#C7E4DF] bg-opacity-50 w-3/5 flex md:flex-row flex-col items-center mx-auto rounded-3xl p-4 my-12">
         <div>
-          <h1 className="font-semibold text-5xl">
+          <h1 className="font-semibold md:text-5xl text-xl">
             Help you find the finest quality of mental health support!
           </h1>
           <p className="md:w-3/4 text-lg mt-4">
@@ -18,8 +18,8 @@ export default function Help() {
         <Image
           src={phonecombine}
           alt="phone-combine"
-          width={300}
-          height={300}
+          width={250}
+          height={250}
         />
       </div>
     </>
